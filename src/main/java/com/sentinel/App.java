bf18f38ebfaf8ad4217ab69840d6786b9c4de29b
bablu kumar <bablu707041dk@gmail.com>
2026-01-07 16:03:09 +0530

    refactor: Implement Multithreading using ScheduledExecutorService

--- src/main/java/com/sentinel/App.java
+++ src/main/java/com/sentinel/App.java
@@ -1,18 +1,35 @@
 package com.sentinel;
 
-import com.sentinel.services.SystemMonitor;
+import com.sentinel.model.SystemMetrics;
+import com.sentinel.services.DataFetcher;
 import com.sentinel.ui.Dashboard;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class App {
-    public static void main(String[] args){
-        SystemMonitor monitor = new SystemMonitor();
+    public static void main(String[] args) {
+        System.out.println("Starting J-Sentinel Engines...");
+
+        // 1. Shared Data Model (Box)
+        SystemMetrics metrics = new SystemMetrics();
+
+        // 2. Background Worker Setup (The Chef)
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        DataFetcher fetcher = new DataFetcher(metrics);
+
+        // Har 1 second me data fetch karo (Background me)
+        scheduler.scheduleAtFixedRate(fetcher, 0, 1, TimeUnit.SECONDS);
+
+        // 3. UI Setup (The Waiter)
         Dashboard dashboard = new Dashboard();
 
-        while(true){
-            dashboard.render(monitor);
-            try{
-                Thread.sleep(1500);
+        while (true) {
+            dashboard.render(metrics);
+
+            try {
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
