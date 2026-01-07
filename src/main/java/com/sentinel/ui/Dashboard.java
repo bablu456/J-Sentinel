e10b817cb2f50eb8e28dff4901e6a3b01eeab681
bablu kumar <bablu707041dk@gmail.com>
2026-01-07 15:44:42 +0530

    Update: just updated the dashboard

--- src/main/java/com/sentinel/ui/Dashboard.java
+++ src/main/java/com/sentinel/ui/Dashboard.java
@@ -6,7 +6,7 @@ import com.sentinel.utils.ConsoleUtils;
 public class Dashboard {
 
     // Ye method data lega aur screen par draw karega
-    public void render(SystemMonitor monitor) {
+    public void render(com.sentinel.model.SystemMetrics metrics) {
         // Screen saaf karo
         ConsoleUtils.clearScreen();
 
@@ -17,13 +17,13 @@ public class Dashboard {
 
         // System Info Section
         System.out.println(ConsoleUtils.YELLOW + "[ SYSTEM DETAILS ]" + ConsoleUtils.RESET);
-        System.out.println("OS Name:    " + monitor.getOsName());
+        System.out.println("OS Name:    " + metrics.getOsName());
         System.out.println("");
 
         // Resources Section (Table format)
         System.out.println(ConsoleUtils.YELLOW + "[ LIVE RESOURCES ]" + ConsoleUtils.RESET);
-        System.out.printf("RAM Usage:  %s%n", monitor.getMemoryInfo());
-        System.out.printf("CPU Load:   %s%n", monitor.getCpuLoad());
+        System.out.printf("RAM Usage:  %s%n", metrics.getRamUsage());
+        System.out.printf("CPU Load:   %s%n", metrics.getCpuLoad());
 
         System.out.println("");
         System.out.println(ConsoleUtils.CYAN + "----------------------------------------" + ConsoleUtils.RESET);
