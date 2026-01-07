be5b2cde24b1ec89c2a0dad3819ed26ad22c72bd
bablu kumar <bablu707041dk@gmail.com>
2026-01-07 15:36:50 +0530

    feat: Create SystemMetrics model for thread-safe data sharing

---
+++ src/main/java/com/sentinel/model/SystemMetrics.java
@@ -0,0 +1,26 @@
+package com.sentinel.model;
+
+public class SystemMetrics {
+    private volatile String ramUsage = "Calculating...";
+    private volatile String cpuLoad = "Calculating...";
+    private volatile String osName = "Calculating...";
+
+    public String getRamUsage() {
+        return ramUsage;
+    }
+    public void setRamUsage(String ramUsage) {
+        this.ramUsage = ramUsage;
+    }
+    public String getCpuLoad() {
+        return cpuLoad;
+    }
+    public void setCpuLoad(String cpuLoad) {
+        this.cpuLoad = cpuLoad;
+    }
+    public String getOsName() {
+        return osName;
+    }
+    public void setOsName(String osName) {
+        this.osName = osName;
+    }
+}
